2f045bca268d0d317c8825ddee916d0dc0b0bbf4
Pavel Kazlou <p.i.kazlou@gmail.com>
2023-06-03 10:13:31 +0300

    refactored accuracy computation into module

---
+++ compute_accuracy.py
@@ -0,0 +1,48 @@
+import os
+import operator
+from typing import List
+
+def load_dataset(path) -> List[tuple[List[str], str]]:
+    result = []
+    for file in os.listdir(path):
+        if file.endswith('.txt'):
+            with open(path + '/' + file) as f:
+                cluster_reading = True
+                cluster = []
+                outliers = []
+                for line in f:
+                    line = line.rstrip()
+                    if len(line) > 0:
+                        if not line.startswith('#'):
+                            if cluster_reading:
+                                cluster.append(line)
+                            else:
+                                outliers.append(line)
+                    else:
+                        cluster_reading = False
+                for outlier in outliers:
+                    result.append((cluster, outlier))
+    return result
+
+
+def compute_accuracy(sim_fun):
+    dataset = load_dataset('evaluation/outlier-detection')
+    similarities = {}
+    correct_guesses = 0
+    total_guesses = 0
+    for sample in dataset:
+        cluster, outlier = sample
+        items = cluster + [outlier]
+        compactness = {}
+        for item1 in items:
+            compactness[item1] = 0.0
+            for item2 in items:
+                if item1 != item2:
+                    compactness[item1] += sim_fun(item1, item2)
+        least_compact = sorted(compactness.items(), key=operator.itemgetter(1))[0][0]
+        if least_compact == outlier:
+            correct_guesses += 1
+        else:
+            print(f"Failed {cluster} / {outlier}")
+        total_guesses += 1
+    return correct_guesses / total_guesses
